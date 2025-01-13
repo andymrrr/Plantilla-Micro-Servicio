7c292d1566fec646984e6ca3c16450b0a9439fe3
andymrrr <42513348+andymrrr@users.noreply.github.com>
2025-01-12 20:47:07 -0400

    Etructura Base e libro para Pagnacion

---
+++ PlantillaMicroServicio.Aplicacion/Paginacion/Libros/PaginacionParaConteoDeLibros.cs
@@ -0,0 +1,17 @@
+﻿using PlantillaMicroServicio.Dal.Nucleo.Paginacion;
+using PlantillaMicroServicio.Modelos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlantillaMicroServicio.Aplicacion.Paginacion.Libros
+{
+    public class PaginacionParaConteoDeLibros : PaginacionBase<Libro>
+    {
+        public PaginacionParaConteoDeLibros(PaginacionLibroParametro parametro)
+         : base(parametro.ConstruirFiltro())
+        { }
+    }
+}
