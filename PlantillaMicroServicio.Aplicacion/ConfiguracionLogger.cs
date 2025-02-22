ff900472bae9ab65f07fbd619d41d41c05db4cd3
Andy Reyes <42513348+andymrrr@users.noreply.github.com>
2025-02-22 16:23:25 -0400

    Configuracion

--- PlantillaMicroServicio.Aplicacion/ConfiguracionLogger.cs
+++ PlantillaMicroServicio.Aplicacion/ConfiguracionLogger.cs
@@ -11,16 +11,11 @@ namespace PlantillaMicroServicio.Aplicacion
         public static void AgregarLogging(WebApplicationBuilder builder)
         {
             // Configurar Serilog usando la configuración de appsettings.json
-            builder.Host.UseSerilog((context, services, configuration) =>
-            {
-                // Configuración de Serilog para registrar solo lo que se maneje en los filtros.
-                configuration
-                    .ReadFrom.Configuration(context.Configuration) // Lee la configuración de appsettings.json
-                    .WriteTo.Console() // Puedes ajustar el destino de log según tus necesidades.
-                    .WriteTo.File(
-                        context.Configuration["SerilogConfig:Path"] ?? "logs/log.txt", // Ruta del archivo de logs desde la configuración, o valor por defecto
-                        rollingInterval: RollingInterval.Day); // Logs se rotarán diariamente
-            });
+            Log.Logger = new LoggerConfiguration()
+             .ReadFrom.Configuration(builder.Configuration)
+              .CreateLogger();
+
+            builder.Host.UseSerilog(Log.Logger);
 
             builder.Services.AddScoped<FiltroRegistroSolicitudes>(); // Agregamos el filtro
             // Inyectar el servicio de LoggerService
